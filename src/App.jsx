bd389f912b32f095df16b375f137e34ae690ea86
Talvarez <soytalvarez17@gmail.com>
2024-08-29 22:35:49 -0600

    Boton de regreso en partida

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import LogList from "./Components/Log";
 import Heading from "./Components/Heading";
 import { SocketContext } from "./context/SocketContext";
 import useGame from "./hooks/useGame";
+import "../src/Components/css/partidas.css"
+import { Link } from "react-router-dom";
 
 const App = () => {
   const socket = useContext(SocketContext);
-  const {myState, opponentState, logState} = useGame(false, socket);
+  const { myState, opponentState, logState } = useGame(false, socket);
 
   return (
     <>
       <Heading />
+      <div className="header">
+        <Link to={"/"} className="back-button">Regresar</Link>
+      </div>
+      
       <Display {...{ myState, opponentState }} />
       <LogList {...logState} />
     </>
